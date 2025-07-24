d467bac04dc85d94a0be7d633956acbb354a13a7
mukund58 <cursedscripting@gmail.com>
2025-07-24 22:54:50 +0530

    add Find Minimum in Rotated Sorted Array

---
+++ LeetCode/0153-Find-Minimum-in-Rotated-Sorted-Array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findMin(vector<int> &nums) {
+        int left = 0;
+        int right = nums.size()-1;
+        int result =nums[0];
+        while(left <=right ){
+                if(nums[left] < nums[right]){
+                    result= min(result,nums[left]);
+                    break;
+                }
+                int mid = left + (right - left ) / 2;
+                result = min( result,nums[mid]);
+
+                if(nums[mid] >= nums[left]) {
+                    left = mid+1;
+                }
+                else right = mid -1;
+                 
+                }
+        return result;
+    }
+};
+
